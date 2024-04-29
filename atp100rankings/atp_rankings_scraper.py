6a94ba970439dc289106231d4c58a7835f5e8dca
Stam Plato <stamplato@gmail.com>
2024-04-29 20:41:23 +0300

    Adding new layers

--- atp100rankings/atp_rankings_scraper.py
+++ atp100rankings/atp_rankings_scraper.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def get_atp_100_info(week_list):
     BASE_URL = "https://www.atptour.com/en/rankings/singles?RankRange=0-100&Region=all&DateWeek"
@@ -32,16 +33,35 @@ def get_atp_100_info(week_list):
         
 
         #Find the player list
+        content_age_player_list = soup.find_all('td', class_ = 'age small-cell')[:100]
+        age_player_list = list(map(lambda x: x.text.strip('\n'),content_age_player_list))
+        
+        #Find the ages of players list
         content_player_list = soup.find_all('li', class_ = 'name center')[:100]
         player_list = list(map(lambda x: x.text.strip('\n'),content_player_list))
-        
 
         #Find total points list
         content_player_points_list = soup.find_all('td', class_ = 'points center bold extrabold small-cell')[:100]
         player_points_list = list(map(lambda x: x.text.strip(' \r\n '),content_player_points_list))
         
+        #Difference of points earned/dropped in current week (+/-)
+        content_difference_points_list = soup.find_all('td', class_ = re.compile(r'^small-cell pointsMove center'))[:100]
+        player_diff_points_list = list(map(lambda x: x.text.strip(' \r\n ') ,content_difference_points_list))
+        
 
-        dict_temp = {'week': [week]* 100, 'ranking':ranking_list, 'country': country_list, 'player': player_list, 'points': player_points_list}
+        
+        #Tournaments played for each player
+        content_tourn_played_list = soup.find_all('td', class_ = 'tourns center small-cell')[:100]
+        player_tourn_played_list = list(map(lambda x: x.text.strip(' \r\n '),content_tourn_played_list))
+        
+        dict_temp = {'week': [week]* 100, 
+                     'ranking':ranking_list, 
+                     'country': country_list, 
+                     'player': player_list, 
+                     'age': age_player_list, 
+                     'points': player_points_list,
+                     '(+/-)': player_diff_points_list,
+                     'tournaments_played':player_tourn_played_list}
         
         df_dict_temp = pd.DataFrame(dict_temp)
         
